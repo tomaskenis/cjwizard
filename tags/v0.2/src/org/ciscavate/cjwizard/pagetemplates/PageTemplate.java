0da6ee93b8189230f0dca2c3fa4f2c04199f37b7
creswick <creswick@2a4f5638-ac50-0410-b0e2-7bef82531098>
2008-07-01 19:03:15 +0000

    significant improvements in documentation and api clarity.

---
+++ tags/v0.2/src/org/ciscavate/cjwizard/pagetemplates/PageTemplate.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package org.ciscavate.cjwizard.pagetemplates;
+
+import javax.swing.JPanel;
+
+import org.ciscavate.cjwizard.WizardController;
+import org.ciscavate.cjwizard.WizardPage;
+
+/**
+ * @author rcreswick
+ *
+ */
+public abstract class PageTemplate extends JPanel {
+
+   /**
+    * Reference to the wizard controller that manages this template.
+    */
+   private WizardController _controller = null;
+   
+   /**
+    * Called when a new page is to be displayed in the wizard.
+    * 
+    * @param page The page to display.
+    */
+   public abstract void setPage(final WizardPage page);
+
+   /**
+    * Registers a WizardController with this class.
+    * 
+    * @param controller
+    */
+   public void registerController(WizardController controller) {
+      _controller = controller;
+   }
+
+   /**
+    * @return the WizardController that manages this wizard.
+    */
+   protected WizardController getController() {
+      return _controller;
+   }
+}
